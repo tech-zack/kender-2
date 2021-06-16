4e72b56aa44c58526ced7315758a02c0970efb78
tech-zack <difference19@icloud.com>
2021-06-16 16:26:27 +0900

    フロント訂正6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'kenders/check'
-  get 'kenders/check2'
-  get 'kenders/check3'
+  get 'kenders/kendo'
+  get 'kenders/kenzyutu'
+  get 'kenders/kendougu'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'kenders#index'
